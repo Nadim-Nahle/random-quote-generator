2b708200f5578361ef686990e99adee5b51ac5a8
Nadim <nahlenadim@gmail.com>
2022-05-18 14:36:51 +0300

    adding the js for generating a new donald trump tweet through API

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,33 @@
 const QuoteButton = document.querySelector('.js-new-quote');
+
 QuoteButton.addEventListener('click', getQuote)
-function getQuote(){
-    console.log("the quote button was clicked")
-}
+
+const endpoint= "https://api.whatdoestrumpthink.com/api/v1/quotes/random"
+
+async function getQuote() {
+    // The `try` block executes the statements within it as usual.
+    // If an exception is thrown, the statements defined in
+    // the `catch` block will be executed.
+    // Learn more here: https://javascript.info/try-catch
+    try {
+      const response = await fetch(endpoint)
+      // If the response is not 200 OK...
+      if (!response.ok) {
+        // ...throw an error. This causes control flow
+        // to skip to the `catch` block below.
+        throw Error(response.statusText)
+      }
+  
+      const json = await response.json();
+      displayQuote(json.message);
+    } 
+    catch (err) {
+      console.log(err)
+      alert('Failed to fetch new quote');
+    }
+
+    function displayQuote(quote) {
+        const quoteText = document.querySelector('.quote1');
+        quoteText.textContent = quote;
+      }
+  }
