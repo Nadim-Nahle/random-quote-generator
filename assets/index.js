e05731d80acae1ff750974d1017bab90ae4e3835
Nadim <nahlenadim@gmail.com>
2022-05-18 13:51:06 +0300

    creating the layout

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+const QuoteButton = document.querySelector('.js-new-quote');
+QuoteButton.addEventListener('click', getQuote)
+function getQuote(){
+    console.log("the quote button was clicked")
+}
